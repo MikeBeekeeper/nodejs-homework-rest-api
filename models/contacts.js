e4eabbf3bf462d024cc2b0b9611c6599ece8346b
MikeBeekeeper <mike.rotar.91@gmail.com>
2023-12-15 10:15:08 +0200

    final

--- models/contacts.js
+++ models/contacts.js
@@ -1,17 +1,54 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path')
+const nanoid = require('nanoid')
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const getContactslist = async () => {
+  const contactList = await fs.readFile(contactsPath);
+  return JSON.parse(contactList);
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contactList = await getContactslist();
+  const contact = contactList.find((item) => item.id === contactId);
+  return contact || null;
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contactList = await getContactslist();
+  const contactIndex = contactList.findIndex((item) => item.id === contactId);
 
-const updateContact = async (contactId, body) => {}
+  if (contactIndex !== -1) {
+    contactList.splice(contactIndex, 1);
+    fs.writeFile(contactsPath, JSON.stringify(contactList, null, 2));
+    return contactList;
+  }
+}
+
+const addContact = async (body) => {
+    const { name, email, phone } = body;
+    const contactList = await getContactslist();
+    const newContact = {id: nanoid(), name, email, phone}
+    contactList.push(newContact)
+    fs.writeFile(contactsPath, JSON.stringify(contactList, null, 2))
+
+    return newContact
+}
+
+const updateContact = async (contactId, body) => {
+  const contactList = await getContactslist();
+  const index = contactList.findIndex(item => item.id === contactId)
+  if (index === -1) {
+    return null
+  }
+  contactList[index] = { contactId, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contactList, null, 2))
+  return contactList[index]
+}
 
 module.exports = {
-  listContacts,
+  getContactslist,
   getContactById,
   removeContact,
   addContact,
