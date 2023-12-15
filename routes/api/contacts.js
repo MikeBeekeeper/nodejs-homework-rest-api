aeaf589d50a76f052cb170aaa6ed2d49439b5d70
MikeBeekeeper <mike.rotar.91@gmail.com>
2023-12-15 10:35:02 +0200

    first

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,6 +10,10 @@ router.get('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
+// router.get('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
+
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
